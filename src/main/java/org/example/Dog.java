eeb34986a21c7011c4081ee72f499b2ad3f9e48e
kunjut <v.dimnich@gmail.com>
2021-02-25 19:24:16 +0300

    DI via setter

--- src/main/java/org/example/Dog.java
+++ src/main/java/org/example/Dog.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Dog implements Pet {
+    public Dog() {
+        System.out.println(">Dog bean is created");
+    }
+
     @Override
     public void say() {
         System.out.println("Bark-bark");
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -3,7 +3,16 @@ package org.example;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+//    public Person(Pet pet) {
+//        this.pet = pet;
+//    }
+
+    public Person() {
+        System.out.println(">Person bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println(">Class person: set pet");
         this.pet = pet;
     }
 
