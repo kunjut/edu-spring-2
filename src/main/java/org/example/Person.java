c2ca46f3712f2787547993df2c65d0187f43a7d8
kunjut <v.dimnich@gmail.com>
2021-03-01 20:34:56 +0300

    Add @Qualifier to @Autowired

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,26 +1,29 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Person {
-    @Autowired
+//    @Autowired
+//    @Qualifier("catBean")
     private Pet pet;
     private String surname;
     private int age;
 
-//    @Autowired
-//    public Person(Pet pet) {
-//        System.out.println("Person bean is created");
-//        this.pet = pet;
-//    }
+    @Autowired
+    public Person(@Qualifier("catBean") Pet pet) {
+        System.out.println("Person bean is created");
+        this.pet = pet;
+    }
 
 //    public Person() {
 //        System.out.println(">Person bean is created");
 //    }
 
 //    @Autowired
+//    @Qualifier("catBean")
     public void setPet(Pet pet) {
         System.out.println(">Class person: set pet");
         this.pet = pet;
