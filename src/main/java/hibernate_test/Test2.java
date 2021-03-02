d7e9f2969815fb6a2edd1d14df2fce4eeb86272e
kunjut <v.dimnich@gmail.com>
2021-03-02 18:32:16 +0300

    Get object from db

--- src/main/java/hibernate_test/Test2.java
+++ src/main/java/hibernate_test/Test2.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+public class Test2 {
     public static void main(String[] args) {
         SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -15,10 +15,22 @@ public class Test1 {
         Session session = sessionFactory.getCurrentSession();
 
         try {
-            Employee emp = new Employee("Aleks","Smirnov","Salary",650);
+            Employee emp = new Employee("Elena","Petrova","Sales",800);
             session.beginTransaction();
             session.save(emp);
             session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+            int myId = emp.getId();
+
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            Employee employee = session.get(Employee.class, myId);
+            session.getTransaction().commit();
+
+            System.out.println(employee);
+
         } finally {
             sessionFactory.close();
         }
